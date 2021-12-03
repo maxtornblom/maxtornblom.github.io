d3051b67b70aeb6d5e750120f7aa54dd78b43bb6
Max Törnblom <90630609+maxtornblom@users.noreply.github.com>
2021-12-03 19:53:59 +0100

    Update

--- TheTests/click/click.js
+++ TheTests/click/click.js
@@ -3,14 +3,18 @@ function click(e) {
     e.preventDefault()
    
     document.getElementById('clicks').className = "hidden"
-    document
+    document.getElementById('mainMeny').className = "hidden"
     
 
     setTimeout(function() {document.onmousedown = undefined;
 
         document.getElementById('clicks').innerHTML = "You clicked with the speed of " + results/5 + " CPS"
                                 document.getElementById('clicks').className = "visible"
-                                document.getElementById('restart').className = "visible"}, 5000);
+                                setTimeout(function() {
+                                    document.getElementById('mainMeny').className = "visible mainMeny"
+                                    document.getElementById('restart').className = "visible restart"  
+                                },500)
+                                }, 5000);
    
     document.getElementById('results').innerHTML = results++
 }
@@ -19,6 +23,10 @@ let results = 0;
 
 
 document.onmousedown = click 
+
+function confirm() {
+    confirm()
+}
 
 
 
