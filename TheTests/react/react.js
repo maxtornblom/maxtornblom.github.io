bcba0e8f6340d58079d955908a2001680d8d13ef
Max Törnblom <90630609+maxtornblom@users.noreply.github.com>
2021-12-12 10:12:08 +0100

    update

--- TheTests/react/react.js
+++ TheTests/react/react.js
@@ -5,35 +5,40 @@ function getRandomNumberBetween(min, max) {
 function measureTime(e) {
     let stopTime = new Date()
     let results = stopTime - startTime
-    document.onclick = undefined
-    document.onmousedown = undefined
+    
     document.getElementById('mainMenyButton').className = "vivible , mainMeny "
     document.getElementById('body').className = "bodyResults";
     document.getElementById('results').innerHTML = results + "  ms";
     document.getElementById('click').className = "hidden";
     document.getElementById('restartButton').className = "vivible , buttonRestart";
-
-   
 } 
 
  
 
 
-//document.onmousedown = alert("you clicked too early")
+
 
 
 
 
 function switchPage() {
 
+    
+        document.onmousedown = 0
+       
+    
+       if(document.onmousedown = 0) {
+           alert("you clicked too early")
+       }
 
     return setTimeout(
         function () {
             document.getElementById("body").className = "green";
-            document.getElementById("click").className = "visible";
+            document.getElementById("click").className = "visible click";
             document.getElementById("wait").className = "hidden";
             document.onmousedown = measureTime
             startTime = new Date()
+
         }, getRandomNumberBetween(1 * 1000, 10 * 1000));
 }
 
