914b4fdfdf845b91eb4c24a2455415037389c020
Max Törnblom <90630609+maxtornblom@users.noreply.github.com>
2021-12-28 15:31:06 +0100

    Click too early was added

--- TheTests/react/react.js
+++ TheTests/react/react.js
@@ -11,9 +11,9 @@ function measureTime(e) {
     document.getElementById('results').innerHTML = results + "  ms";
     document.getElementById('click').className = "hidden";
     document.getElementById('restartButton').className = "vivible , buttonRestart";
-} 
+}
 
- 
+
 
 
 
@@ -23,8 +23,12 @@ function measureTime(e) {
 
 function switchPage() {
 
-    return setTimeout(
-        function () {
+    let timerHasStarted = false;
+    document.getElementById('before').className = "hidden"
+
+    const timerId = setTimeout(
+        function change() {
+            timerHasStarted = true;
             document.getElementById("body").className = "green";
             document.getElementById("click").className = "visible click";
             document.getElementById("wait").className = "hidden";
@@ -32,5 +36,23 @@ function switchPage() {
             startTime = new Date()
 
         }, getRandomNumberBetween(1 * 1000, 10 * 1000));
-}
 
+    
+
+    document.addEventListener("click", function () {
+        if (timerHasStarted === false) {
+            document.getElementById('before').className = "visible toSoon"
+            document.getElementById('agin').className = "restartAgin"
+            console.log("stopping timer",timerId);
+            clearTimeout(timerId);
+            document.getElementById('body').className = "bodyResults"
+            document.getElementById('wait').className = "hidden"
+
+            document.addEventListener('click', function () {
+                window.location.href = "/TheTests/react/react.html";
+            })
+        }  
+            
+        
+    })
+}
